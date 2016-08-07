ee9062b641b04e1430d3eddec1b0cf8a13b85f6f
XxlolensxX <ulises.arratia@gmail.com>
2016-08-07 15:17:37 -0300

    agregado Browser-Sync

--- gulpFile.js
+++ gulpFile.js
@@ -4,10 +4,17 @@ const gulp = require('gulp'),
     rename = require('gulp-rename'),
     postcss = require('gulp-postcss'),
     cssnano = require('cssnano'),
-    liveReload = require('gulp-livereload');
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    liveReload = require('gulp-livereload'),
+    concat = require('gulp-concat');
 
 
 //tareas de desarrollo
+
+/*-------------------------------
+/////////////SASS////////////////
+-------------------------------*/
 gulp.task('sass', ()=>
     gulp.src('./scss/*.sass')
         .pipe(sass({
@@ -20,6 +27,30 @@ gulp.task('sass', ()=>
         .pipe(gulp.dest('../style.css'))
         
 );//primer parametro nombre de la tarea, funcion con los otros metodos src o dest
+/*-------------------------------
+/////////////SASS////////////////
+-------------------------------*/
+
+/*-------------------------------
+/////////BROWSER-SYNC////////////
+-------------------------------*/
+gulp.task('browserSync', function(){
+    var archivos = [
+        '../*.css',
+        '../*.php'
+    ];
+//inicializar BROWSER-SYNC
+    browserSync.init(archivos,{
+        proxy: 'http://localhost:8888/portafolio/',
+        notify: false
+    });
+});
+    
+
+/*-------------------------------
+/////////BROWSER-SYNC////////////
+-------------------------------*/
+
 
 //tareas de producción
 gulp.task('postcss', function(){
@@ -33,5 +64,5 @@ gulp.task('postcss', function(){
 });
 
 gulp.task('default', ()=> {
-    gulp.watch('./scss/*.sass', ['sass'])
+    gulp.watch('./scss/*.sass', ['sass','browserSync'])
 });
